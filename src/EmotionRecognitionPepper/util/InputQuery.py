697bd50bb8bb61ef3fac738443ee08415f2caaa8
Ajtofie <matej.stupar@web.de>
2022-10-11 23:48:12 +0200

    -

--- src/EmotionRecognitionPepper/util/InputQuery.py
+++
@@ -1,6 +0,0 @@
-def input_query(query_statement):
-    answer = raw_input("Do you want to " + query_statement + "? [y|n]")
-    if answer == ("y" or "Y"):
-        return True
-    else:
-        return False
--- src/EmotionRecognitionPepper/util/Timer.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-# Import libraries
-import time
-
-
-def print_passed_time(start_time):
-    return "--- %s seconds ---" % round(time.clock() - start_time, 4)
-
